ab538d6965ae3650d24cc7c4af0c6c67534a33ff
ddavidov <ddavidov@mindk.com>
2016-11-15 14:32:06 +0200

    feat(data): Json class tests added

---
+++ zoolanders/tests/Data/JsonTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ZFTests\Data;
+
+use ZFTests\TestCases\ZFTestCase;
+use Zoolanders\Framework\Data\Json;
+
+/**
+ * Class JsonTest
+ * Json data tests
+ *
+ * @package ZFTests\Data
+ */
+class JsonTest extends ZFTestCase
+{
+    /**
+     * Test create Json
+     */
+    public function testCreateJson(){
+
+        $this->markTestSkipped('Contains no signifficant methods differences');
+    }
+}

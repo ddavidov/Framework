6dd6e0948657d7c123e35623087a298cba975820
ddavidov <ddavidov@mindk.com>
2016-11-16 10:46:25 +0200

    feat(data): Parameter class tests improved

--- zoolanders/tests/Data/ParameterTest.php
+++ zoolanders/tests/Data/ParameterTest.php
@@ -22,4 +22,50 @@ class ParameterTest extends DataTest
         return $this->object;
     }
 
+    /**
+     * Test loadArray method
+     *
+     * @covers          Parameter::loadArray()
+     * @dataProvider    arrayDataSet
+     */
+    public function testLoadArray($dataset, $control_value){
+        $this->makeDataSet([]);
+        $this->object->loadArray($dataset);
+
+        // Check if control element is present:
+        $this->assertTrue($this->object->has($control_value));
+    }
+
+    /**
+     * Test loadObject method
+     *
+     * @covers          Parameter::loadObject()
+     * @dataProvider    objectDataSet
+     */
+    public function testLoadObject($dataset, $control_prop, $control_value){
+        $this->makeDataSet([]);
+        $this->object->loadObject($dataset);
+
+        // Check if control element is present and value is correct:
+        $this->assertTrue($this->object->has($control_prop));
+        $this->assertEquals($control_value, $this->object->get($control_prop));
+    }
+
+    /**
+     * Object dataset
+     */
+    public function objectDataSet(){
+
+        $object = new \stdClass();
+        $object->a = 'alpha';
+        $object->b = 'bravo';
+        $object->c = 'charlie';
+
+        return [
+          [ $object, 'a', 'alpha' ],
+          [ $object, 'b', 'bravo' ],
+          [ $object, 'c', 'charlie' ]
+        ];
+    }
+
  }

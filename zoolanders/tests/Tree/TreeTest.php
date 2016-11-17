1bb7c68e09bd2825955248d8d6710f7006d1d33d
ddavidov <ddavidov@mindk.com>
2016-11-17 14:22:00 +0200

    feat(model): Model class covered with tests

---
+++ zoolanders/tests/Tree/TreeTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ZFTests\Tree;
+use ZFTests\TestCases\ZFTestCase;
+use Zoolanders\Framework\Tree\Tree;
+
+/**
+ * Class TreeTest
+ * @package ZFTests\Tree
+ */
+class TreeTest extends ZFTestCase
+{
+    /**
+     * Test tree creation
+     */
+    public function testMakeTree(){
+        //@TODO: Implement later. Test cases could be non-informative at the moment
+        $this->markTestSkipped('To be implemented later');
+    }
+}

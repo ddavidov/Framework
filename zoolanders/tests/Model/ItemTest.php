a4b3b1b241c12563f777067536a442ff117b695f
ddavidov <ddavidov@mindk.com>
2016-11-17 16:23:17 +0200

    feat(model): Databese Item test (marked as skipped)

---
+++ zoolanders/tests/Model/ItemTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ZFTests\Model;
+
+use ZFTests\TestCases\ZFTestCase;
+
+/**
+ * Class ItemTest
+ * Item test
+ *
+ * @package ZFTests\Model
+ */
+class ItemTest extends ZFTestCase
+{
+    /**
+     * Test creating instance
+     */
+    public function testMake(){
+        //@TODO: Implement this
+        $this->markTestSkipped('To be implemented afetr query builder updated');
+    }
+}

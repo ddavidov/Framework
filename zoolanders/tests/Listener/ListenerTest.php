95e744e1b4582dd2b1e8613ec719023b22ac655c
ddavidov <ddavidov@mindk.com>
2016-12-08 11:31:09 +0200

    fix(listeners): Dummy test added (no logics, abstract class)

---
+++ zoolanders/tests/Listener/ListenerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ZFTests\Listener;
+
+use ZFTests\TestCases\ZFTestCase;
+use Zoolanders\Framework\Listener\Listener;
+
+/**
+ * Class ListenerTest
+ * Listener class tests
+ *
+ * @package ZFTests\Listener
+ */
+class ListenerTest extends ZFTestCase
+{
+    /**
+     * Test listener
+     */
+    public function testListener(){
+        $this->markTestSkipped('Listener is abstract and have no core logic');
+    }
+}

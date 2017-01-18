51bb9750b60b1b07d045e46347f6b3848ceef518
ddavidov <ddavidov@mindk.com>
2017-01-18 12:57:37 +0200

    feat(controllers): COntroller tests updated

--- zoolanders/tests/Controller/ControllerTest.php
+++ zoolanders/tests/Controller/ControllerTest.php
@@ -3,6 +3,7 @@
 namespace ZFTests\Controller;
 
 use ZFTests\TestCases\ZFTestCase;
+use ZFTests\Classes\TestController;
 
 /**
  * Class ControllerTest
@@ -14,10 +15,42 @@ class ControllerTest extends ZFTestCase
 {
     /**
      * Test controller init
+     *
+     * @covers          Controller::execute()
+     * @dataProvider    actionMap
      */
-    public function testMakeController(){
-        $this->markTestSkipped(
-            'Specific assertions missing'
-        );
+    public function testExecute($action, $response){
+        $ctrl = new TestController(self::$container);
+
+        // Check expected response
+        $this->assertEquals($response, $ctrl->execute($action));
+
+        // Check if expected events were triggered
+        $this->assertEventTriggered('controller:beforeexecute', function(){});
+        $this->assertEventTriggered('controller:afterexecute', function(){});
+    }
+
+    /**
+     * Test redirect flow
+     *
+     * @covers      Controller::setRedirect()
+     * @covers      Controller::hasRedirect()
+     */
+    public function testRedirects(){
+        $ctrl = new TestController(self::$container);
+
+        $ctrl->setRedirect('/');
+
+        $this->assertTrue($ctrl->hasRedirect());
+    }
+
+    /**
+     * Actions and expected responses mapping set
+     */
+    public function actionMap(){
+        return [
+            ['ping', 'test'],
+            ['_default', 'default']
+        ];
     }
 }

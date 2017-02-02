1f67f192caea9c0491b872d42fb94c8894431c96
Daniele Rosario <realskullbock@gmail.com>
2017-02-02 15:29:33 +0100

    Separated Csrf Protection into a trait

--- zoolanders/Framework/Controller/Controller.php
+++ zoolanders/Framework/Controller/Controller.php
@@ -3,7 +3,6 @@
 namespace Zoolanders\Framework\Controller;
 
 use Zoolanders\Framework\Container\Container;
-use Zoolanders\Framework\Controller\Exception\AccessForbidden;
 use Zoolanders\Framework\Controller\Exception\TaskNotFound;
 use Zoolanders\Framework\Controller\Mixin\HasRedirects;
 use Zoolanders\Framework\Event\Controller\AfterExecute;
@@ -168,48 +167,6 @@ class Controller
         $this->defaultTask = $method;
 
         return $this;
-    }
-
-    /**
-     * Provides CSRF protection through the forced use of a secure token. If the token doesn't match the one in the
-     * session we return false.
-     *
-     * @return  bool
-     *
-     * @throws  \Exception
-     */
-    protected function csrfProtection()
-    {
-        $hasToken = false;
-        $session = $this->container->system->getSession();
-
-        // Joomla! 2.5+ (Platform 12.1+) method
-        if (method_exists($session, 'getToken')) {
-            $token = $session->getToken();
-            $hasToken = $this->input->get($token, false, 'none') == 1;
-
-            if (!$hasToken) {
-                $hasToken = $this->input->get('_token', null, 'none') == $token;
-            }
-        }
-
-        // Joomla! 2.5+ formToken method
-        if (!$hasToken) {
-            if (method_exists($session, 'getFormToken')) {
-                $token = $session->getFormToken();
-                $hasToken = $this->input->get($token, false, 'none') == 1;
-
-                if (!$hasToken) {
-                    $hasToken = $this->input->get('_token', null, 'none') == $token;
-                }
-            }
-        }
-
-        if (!$hasToken) {
-            throw new AccessForbidden(403, \JText::_('COM_ZOOLANDERS_ACCESS_FORBIDDEN'));
-        }
-
-        return true;
     }
 
     /**
---
+++ zoolanders/Framework/Controller/Mixin/NeedsCsrfProtection.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Zoolanders\Framework\Controller\Mixin;
+
+use Zoolanders\Framework\Controller\Exception\AccessForbidden;
+
+trait NeedsCsrfProtection
+{
+    /**
+     * Provides CSRF protection through the forced use of a secure token. If the token doesn't match the one in the
+     * session we return false.
+     *
+     * @return  bool
+     *
+     * @throws  \Exception
+     */
+    protected function csrfProtection()
+    {
+        $hasToken = false;
+        $session = $this->container->system->getSession();
+
+        // Joomla! 2.5+ (Platform 12.1+) method
+        if (method_exists($session, 'getToken')) {
+            $token = $session->getToken();
+            $hasToken = $this->input->get($token, false, 'none') == 1;
+
+            if (!$hasToken) {
+                $hasToken = $this->input->get('_token', null, 'none') == $token;
+            }
+        }
+
+        // Joomla! 2.5+ formToken method
+        if (!$hasToken) {
+            if (method_exists($session, 'getFormToken')) {
+                $token = $session->getFormToken();
+                $hasToken = $this->input->get($token, false, 'none') == 1;
+
+                if (!$hasToken) {
+                    $hasToken = $this->input->get('_token', null, 'none') == $token;
+                }
+            }
+        }
+
+        if (!$hasToken) {
+            throw new AccessForbidden(403, \JText::_('COM_ZOOLANDERS_ACCESS_FORBIDDEN'));
+        }
+
+        return true;
+    }
+}
